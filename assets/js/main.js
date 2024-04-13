44918d1647c361e0d018a9b4a1b47a5440056212
Mahmoud Samhoud <m.samhoud@hotmail.com>
2024-04-13 10:51:53 +0200

    link email to the contact section

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,6 +109,43 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  //serviceID - TemplateID - #form - publicKey
+  emailjs
+    .sendForm(
+      "service_u20uoua",
+      "template_w9msg9d",
+      "#contact-form",
+      "MlJ0JqwrxFxoY_e_j"
+    )
+
+    .then(
+      () => {
+        //Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+        //remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
+
+        //Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        //Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 // get all sections that have an id defined
 const sections = document.querySelectorAll("section[id]");
